f6c4588839b6982dfb095604a2cb7687292589c1
Thish4 <Thishanth.Thirumurugan@student.uts.edu.au>
2023-10-18 11:42:47 +1100

    Update Camera.m

--- Camera.m
+++ Camera.m
@@ -1,17 +1,17 @@
-//Drawing data from the camera
+% //Drawing data from the camera
 
-//CameraReading = rossubscriber("/camera/color/image_raw/compressed");
+% //CameraReading = rossubscriber("/camera/color/image_raw/compressed");
 
 rgbSub = rossubscriber("/camera/color/image_raw/compressed");
 pause(1);
 image_h = imshow(readImage(rgbSub.LatestMessage));   %take a pic
 
-//Make the figure large
+% //Make the figure large
 set(gcf,'units','normalized','outerposition',[0 0 1 1]);
 
 
-// if you want to take a video
-//Now loop through and update the image data with the latest image data (ctrl + c to stop)
+% // if you want to take a video
+% //Now loop through and update the image data with the latest image data (ctrl + c to stop)
 
 tic
 while 1
